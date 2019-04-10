45ffade2ac693fb3a0b052257e0708d7571e382c
Tobiloba Adejumo <eniolaplus08@gmail.com>
2019-04-10 08:52:49 +0100

    first ocmmit

---
+++ app/src/main/java/com/getmobileltd/testwithandroidx/TestingModule.java
@@ -0,0 +1,11 @@
+package com.getmobileltd.testwithandroidx;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by themavencoder on 10,April,2019
+ */
+@GlideModule
+public class TestingModule extends AppGlideModule {
+}
